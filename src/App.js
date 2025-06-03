568c2f266eaf85ef449f3bc96acf789289d6677a
R Memon <parvez.r.memon.accenture@gmail.com>
2025-06-03 16:34:44 +0530

    Added sample UI with material ui library with pages like Chat, Home and Calendar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import "./App.css";
+import { Box, Typography } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import FooterNav from "./components/FooterNav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React PWA with OneSignal (CDN)</h1>
-        <p>Install and enable notifications for full experience.</p>
-      </header>
-    </div>
+    <Router>
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        {/* Header */}
+        <Box sx={{ p: 2, bgcolor: "primary.main", color: "white" }}>
+          <Typography variant="h6">My PWA Experience</Typography>
+        </Box>
+
+        {/* Main content */}
+        <Box sx={{ flexGrow: 1, p: 3, overflowY: "auto" }}>
+          <AppRoutes />
+        </Box>
+
+        {/* Footer navigation */}
+        <FooterNav />
+      </Box>
+    </Router>
   );
 }
 
